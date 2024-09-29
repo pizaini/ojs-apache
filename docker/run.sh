28be1d0e934e8e44b9e6b0495e23956228ddb66c
Pizaini <zheiner.taurus@gmail.com>
2024-09-29 13:13:44 +0700

    disable change permission

--- docker/run.sh
+++ docker/run.sh
@@ -20,13 +20,6 @@ echo "Change directory owner..."
 chown www-data:www-data -R /var/www/html
 chown www-data:www-data -R /var/www/ojs-data
 
-echo "Change directory permissions..."
-#Set all directory default as 750
-find /var/www/html -type d -exec chmod 750 {} \;
-
-#Set all files default as 640
-find /var/www/html -type f -exec chmod 640 {} \;
-
 #start services
 echo "Start services..."
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
